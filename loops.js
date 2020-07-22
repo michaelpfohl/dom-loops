31909781a00c6262cf191fa83a765e3cf29ac0dd
Michael Pfohl <rmpfohl@gmail.com>
2020-07-21 20:01:37 -0500

    practice loops

---
+++ loops.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const colorLoop = (colors) => {
+    let domString = "";
+
+    for (let i = 0; i < colors.length; i++) {
+        domString += `<h1>${colors[i]}</h1>`;
+    }
+
+    console.log(domString);
+}
+
+colorLoop(colors);
